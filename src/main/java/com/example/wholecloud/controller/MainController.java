e650f5bcaf3706f82e5e211b0426fa68a0d7e156
wogns1026 <sksmsljh1026@gmail.com>
2021-02-26 17:23:21 +0900

    빌드 확인

--- src/main/java/com/example/wholecloud/controller/MainController.java
+++ src/main/java/com/example/wholecloud/controller/MainController.java
@@ -1,7 +1,16 @@
 package com.example.wholecloud.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/")
 public class MainController {
+
+    @GetMapping
+    public String func(){
+        return "hello world";
+    }
+
 }
